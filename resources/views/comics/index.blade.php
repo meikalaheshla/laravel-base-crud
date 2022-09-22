15974a70a572f4b66e8dfb9efcc15933405eab4a
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-22 23:47:40 +0200

    created graphic for index page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,10 +1,36 @@
 @extends('layouts.main')
 @section('title', 'Comics')
 @section('content')
+
 <div class="container">
-    <div class="card">
-        @dump($comics)
-        ciao
+    <div class="row">
+        @forelse($comics as $comic)
+        <div class="col-3">
+    <div class="card mb-3">
+        @if($comic->thumb)
+        <img class="card-img-top" src="{{$comic->thumb}}" alt="{{$comic->title}}">
+        @endif
+        <div class="card-body">
+        <h5 class="card-title">{{$comic->title}}</h5>
+        <h5 class="card-subtitle">{{$comic->series}}</h5>
+        <h5 class="card-subtitle">{{$comic->sale_date}}</h5>
+        <h5 class="card-subtitle">{{$comic->type}}</h5>
+
+        <p class="card-text text-truncate">{{$comic->description}}</p>
+        </div>
+        <div class="card-footer">
+        <small class="text-muted">${{$comic->price}}</small>
+        
+        </div>
     </div>
 </div>
+ @empty
+ <div class="card">
+    <h1>NON CI SONO FUMETTI DISPONIBILI</h1>
+ </div>
+ @endforelse
+        </div>
+    </div>
+</div>
+
 @endsection
