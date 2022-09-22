9d29aeed6250d91eceb3a80f9455b89d64e4006a
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-22 23:17:37 +0200

    created header link to index page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,6 +11,9 @@
       <li class="nav-item  @if(Route::is('home')) active @endif">
         <a class="nav-link" href="{{url('/')}}">Home</a>
       </li>
+      <li class="nav-item  @if(Route::is('comics')) active @endif">
+        <a class="nav-link" href="{{route('comics.index')}}">Comics</a>
+      </li>
       
       
       
