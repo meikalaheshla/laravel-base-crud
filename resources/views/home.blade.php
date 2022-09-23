f763b26ed4d276bd574be591a49ea0c8bf782c1d
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-23 04:04:26 +0200

    added home h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 @section('title', 'Homecomics')
 @section('content')
-
+<div class="text-center">
+    <h1>HOME</h1>
+</div>
 @endsection                                                                                       
